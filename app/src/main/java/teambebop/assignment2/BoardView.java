8c35d27d7d74dc8aa080e7693a92277c6f304da1
D4RK354B3R <d4rk3.54b3r@gmail.com>
2017-04-23 20:09:22 -0700

    Added BoardView.java Changed the program entry to build a new surface

---
+++ app/src/main/java/teambebop/assignment2/BoardView.java
@@ -0,0 +1,70 @@
+package teambebop.assignment2;
+
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+
+/**
+ * Created by d4rk3_000 on 4/23/2017.
+ */
+
+public class BoardView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private Bitmap icon;
+
+    private boolean[][] grids;
+    
+    BoardView(Context context) {
+        //Constructor for the surface
+        super(context);
+        getHolder().addCallback(this);
+        setFocusable(true);
+        setWillNotDraw(false);
+        System.out.println("Constructor");
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        //Called when drawing shit.
+        System.out.println("onDraw()");
+        canvas.drawColor(Color.WHITE);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        //Initialization for when the surface is created
+        System.out.println("SurfaceCreated()");
+        icon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        //Surface changed. Like when changing between landscape and portrait modes.
+
+    }
+
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        //For clean up
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //Event listening
+
+        System.out.println("touch event");
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+        }
+
+        return false;
+    }
+}
--- app/src/main/java/teambebop/assignment2/MainActivity.java
+++ app/src/main/java/teambebop/assignment2/MainActivity.java
@@ -1,13 +1,15 @@
 package teambebop.assignment2;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new BoardView(this));
     }
 }
