a5a7791e5ad493cacd76506bba1bb7cae6406409
Byron <buaguila@uci.edu>
2017-04-25 19:00:08 -0700

    updated android manifest

--- app/src/main/java/teambebop/assignment2/Candy.java
+++ app/src/main/java/teambebop/assignment2/Candy.java
@@ -1,6 +1,9 @@
 package teambebop.assignment2;
 
+import android.graphics.Bitmap;
 import android.graphics.Rect;
+import android.widget.ImageView;
+
 
 /**
  * Created by Byron on 4/23/2017.
@@ -10,11 +13,12 @@ public class Candy {
 
     String color;
     Rect rect;
+    Bitmap icon;
 
-    public Candy(String _color, Rect _rect){
+    public Candy(String _color, Rect _rect, Bitmap _icon){
         this.color = _color;
         this.rect = _rect;
-
+        this.icon = _icon;
 
     }
 
