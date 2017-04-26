5d2b4db0d4c4f5a6ed23ae3bdd0356ff6952dcfd
Byron <buaguila@uci.edu>
2017-04-25 18:22:05 -0700

    updated candy

--- app/src/main/java/teambebop/assignment2/Candy.java
+++ app/src/main/java/teambebop/assignment2/Candy.java
@@ -1,10 +1,23 @@
 package teambebop.assignment2;
 
+import android.graphics.Rect;
+
 /**
  * Created by Byron on 4/23/2017.
  */
 
 public class Candy {
+
+    String color;
+    Rect rect;
+
+    public Candy(String _color, Rect _rect){
+        this.color = _color;
+        this.rect = _rect;
+
+
+    }
+
 
     /* it does everything *** */
 }
